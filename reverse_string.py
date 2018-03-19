3e1a76af58a1c17a064798d5733d6268670e968d
Anant Kaushik <anant.kaushik2@gmail.com>
2018-03-19 19:39:26 +0530

    function that takes a string as input and returns the string reversed

---
+++ reverse_string.py
@@ -0,0 +1,11 @@
+"""
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+
+Run Time: 48ms
+"""
+class Solution:
+    def reverseString(self, s):
+        return s[::-1]
