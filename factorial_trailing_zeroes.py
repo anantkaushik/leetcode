fd2f0a6f80a45f6b4acfeaa33b3a82ec404b23e6
Anant Kaushik <anant.kaushik2@gmail.com>
2018-03-19 20:20:01 +0530

    Given an integer n, return the number of trailing zeroes in n!

---
+++ factorial_trailing_zeroes.py
@@ -0,0 +1,8 @@
+"""Given an integer n, return the number of trailing zeroes in n!."""
+class Solution:
+    def trailingZeroes(self, n):
+        count = 0
+        while n:
+            n //= 5
+            count += n
+        return count
