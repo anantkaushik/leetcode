2f546cc77daf039e2ab0b41b21bb1a23e336d73e
Anant Kaushik <anant.kaushik2@gmail.com>
2018-04-11 19:59:48 +0530

    Move all 0 to the end of list while maintaining the relative order of the non-zero elements

---
+++ move_zeroes.py
@@ -0,0 +1,15 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order 
+of the non-zero elements.
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+"""
+class Solution:
+    def moveZeroes(self, nums):
+        n = nums.count(0)
+        for i in range(n):
+            nums.remove(0)
+            nums.append(0)
