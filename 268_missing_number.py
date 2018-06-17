f261097289744bacae7e28b4aa1cdbb5679d9a56
Anant Kaushik <anant.kaushik2@gmail.com>
2018-06-17 16:24:16 +0530

    find the missing element from the array

---
+++ 268_missing_number.py
@@ -0,0 +1,16 @@
+"""
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+find the one that is missing from the array.
+
+Example 1:
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+"""
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        return (n*(n+1))/2 - sum(nums)
