60283c1ec8d93a023a8da88fdd1ac3757f0f4bab
Anant Kaushik <anant.kaushik2@gmail.com>
2018-09-21 10:57:27 +0530

    Hamming Distance

---
+++ 461-hamming-distance.py
@@ -0,0 +1,23 @@
+"""
+Problem Link: https://leetcode.com/problems/hamming-distance/description/
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+Given two integers x and y, calculate the Hamming distance.
+
+Example:
+Input: x = 1, y = 4
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
+"""
+class Solution(object):
+    def hammingDistance(self, x, y):
+        hD = 0
+        for i in bin(x^y):
+            if i == '1':
+                hD += 1
+        return hD
