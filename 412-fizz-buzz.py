def37cd7226b03bb82166c47340bc72520ff1762
Anant Kaushik <anant.kaushik2@gmail.com>
2018-10-17 23:41:03 +0530

    Fizz Buzz

--- 412-fizz-buzz.py
+++ 412-fizz-buzz.py
@@ -1,6 +1,7 @@
 """
-Write a program that outputs the string representation of numbers from 1 to n.
+Problem Link: https://leetcode.com/problems/fizz-buzz/
 
+Write a program that outputs the string representation of numbers from 1 to n.
 But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output 
 “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
 
