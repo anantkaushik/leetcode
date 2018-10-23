b71139cbd4eb32cf957290de0c82962d9a5e0e77
Anant Kaushik <anant.kaushik2@gmail.com>
2018-10-23 23:46:04 +0530

    Jewels and Stones

--- 771-jewels-and-stones.py
+++ 771-jewels-and-stones.py
@@ -1,4 +1,5 @@
 """
+Problem Link: https://leetcode.com/problems/jewels-and-stones
 You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
 Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
