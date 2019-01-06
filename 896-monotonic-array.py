3576a7c819819cd3d213a5b3aa57ac0d293714a4
anantkaushik <anant.kaushik2@gmail.com>
2019-01-06 23:15:55 +0530

    Monotonic Array

---
+++ 896-monotonic-array.py
@@ -0,0 +1,44 @@
+"""
+Problem Link: https://leetcode.com/problems/monotonic-array/
+
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+Return true if and only if the given array A is monotonic.
+
+Example 1:
+Input: [1,2,2,3]
+Output: true
+
+Example 2:
+Input: [6,5,4,4]
+Output: true
+
+Example 3:
+Input: [1,3,2]
+Output: false
+
+Example 4:
+Input: [1,2,4,5]
+Output: true
+
+Example 5:
+Input: [1,1,1]
+Output: true
+
+Note:
+1 <= A.length <= 50000
+-100000 <= A[i] <= 100000
+"""
+class Solution(object):
+    def isMonotonic(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        increasing = decreasing = True
+        for i in range(len(A) - 1):
+            if A[i] > A[i+1]:
+                increasing = False
+            if A[i] < A[i+1]:
+                decreasing = False
+        return increasing or decreasing
