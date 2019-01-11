e80c4d4111b51cc2bb0e1cdecdd7f0e6989257b6
anantkaushik <anant.kaushik2@gmail.com>
2019-01-11 17:02:33 +0530

    Decode String

---
+++ 394-decode-string.py
@@ -0,0 +1,39 @@
+"""
+Problem Link: https://leetcode.com/problems/decode-string/
+
+Given an encoded string, return it's decoded string.
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly 
+k times. Note that k is guaranteed to be a positive integer.
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those 
+repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Examples:
+s = "3[a]2[bc]", return "aaabcbc".
+s = "3[a2[c]]", return "accaccacc".
+s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+"""
+class Solution:
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        num = 0
+        curString = ''
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(num)
+                curString = ''
+                num = 0
+            elif c == ']':
+                curNum = stack.pop()
+                prevStr = stack.pop()
+                curString = prevStr + curNum*curString
+            elif c.isnumeric():
+                num = num*10 + int(c)
+            else:
+                curString += c
+        return curString
