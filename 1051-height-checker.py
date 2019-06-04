7263abf3c3900e6a4d615c1030a77d4a577cef1b
anantkaushik <anant.kaushik2@gmail.com>
2019-06-04 23:34:47 +0530

    Height Checker

---
+++ 1051-height-checker.py
@@ -0,0 +1,30 @@
+"""
+Problem Link: https://leetcode.com/problems/height-checker/
+
+
+Students are asked to stand in non-decreasing order of heights for an annual photo.
+Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all 
+students to be standing in non-decreasing order of height.)
+
+Example 1:
+Input: [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+Students with heights 4, 3 and the last 1 are not standing in the right positions.
+ 
+Note:
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+"""
+class Solution(object):
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        s = sorted(heights)
+        count = 0
+        for i, h in enumerate(heights):
+            if h != s[i]:
+                count += 1
+        return count
