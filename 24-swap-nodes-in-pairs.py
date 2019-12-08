9fcdf5a89624556df8f393665a9154aa503f31c3
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-08 16:32:13 +0530

    Swap Nodes in Pairs

--- 24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs.py
@@ -8,10 +8,10 @@ Example:
 Given 1->2->3->4, you should return the list as 2->1->4->3.
 """
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def swapPairs(self, head):
@@ -19,23 +19,38 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # if not head:
-        #     return None
-        # cur = head.next if head.next else head
-        # prev = None
-        # while head and head.next:
-        #     temp = head.next
-        #     head.next = head.next.next
-        #     temp.next = head
-        #     head = head.next
-        #     if prev:
-        #         prev.next = temp
-        #     prev = temp.next
-        # return cur
-        if head is None or head.next is None:
+        if not head:
+            return None
+        cur = head.next if head.next else head
+        prev = None
+        while head and head.next:
+            temp = head.next
+            head.next = head.next.next
+            temp.next = head
+            head = head.next
+            if prev:
+                prev.next = temp
+            prev = temp.next
+        return cur
+
+    # With using dummy variable
+    def swapPairs_alt(self, head: ListNode) -> ListNode:
+      newHead = ListNode(0)
+      newHead.next = head
+      cur = newHead
+      while cur.next and cur.next.next:
+        a = cur.next
+        b = cur.next.next
+        cur.next = b
+        a.next = b.next
+        b.next = a
+        cur = a
+      return newHead.next
+
+    def swapPairs_recursive(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
             return head
-        nextHead = head.next.next
-        newHead = head.next
-        head.next.next = head
-        head.next = self.swapPairs(nextHead)
-        return newHead
+        new_start = head.next.next
+        head, head.next = head.next, head
+        head.next.next = self.swapPairs_recursive(new_start)
+        return head    
