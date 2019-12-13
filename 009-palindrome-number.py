5c67ef569470aca5c8793370c2aa038da5e3c3a2
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-13 21:51:42 +0530

    Update 009-palindrome-number.py

--- 009-palindrome-number.py
+++ 009-palindrome-number.py
@@ -31,3 +31,15 @@ class Solution:
             rev = rev * 10 + temp%10
             temp //=10
         return True if x == rev else False
+    
+    def isPalindrome_alternative(self, x: int) -> bool:
+      # If no is negative
+      # If last digit is 0 and no is not 0
+      if x < 0  or (x % 10 == 0 and x != 0):
+        return False
+      rev = 0
+      while x > rev:
+        rev = (rev*10) + (x%10)
+        x //= 10
+      # When the length is an odd number, we can get rid of the middle digit by rev/10
+      return rev == x or rev//10 == x
