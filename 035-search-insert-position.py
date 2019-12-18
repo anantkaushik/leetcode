27ad12a8af8bebcd5ab95711fb133cd2a63d0264
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-19 00:34:55 +0530

    Search Insert Position

--- 035-search-insert-position.py
+++ 035-search-insert-position.py
@@ -13,21 +13,14 @@ Input: [1,3,5,6], 0
 Output: 0
 """
 class Solution:
-    def binarysearch(self,nums,target):
-        first = 0
-        last = len(nums)-1
-        middle = int((first+last)/2)
-        while first<= last:
-            if nums[middle] < target:
-                first = middle + 1
-            elif nums[middle] == target:
-                return middle
-            else:
-                last = middle - 1
-            middle = int((first+last)/2)
-            if first > last:
-                return first
-                
-    def searchInsert(self, nums, target):
-        p = self.binarysearch(nums, target)
-        return p
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+          mid = (start+end)//2
+          if nums[mid] == target:
+            return mid
+          elif nums[mid] < target:
+            start = mid + 1
+          else:
+            end = mid - 1
+        return start
