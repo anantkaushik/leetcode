8a5f7b947a0d79ed1704ae8778b118ab0d700b22
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-22 18:05:50 +0530

    Intersection of Two Arrays II

--- 350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii.py
@@ -21,19 +21,57 @@ What if nums1's size is small compared to nums2's size? Which algorithm is bette
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements 
 into the memory at once?
 """
-class Solution(object):
-    def intersect(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        dic = {}
-        for a in nums1:
-            dic[a] = dic.get(a, 0) + 1
+class Solution:
+    # Time Complexity - O(m+n)
+    # Space Complexity - O(min(m,n))
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums2) > len(nums1):
+          # it will reduce the space complexity to O(min(m,n)) 
+          self.intersect(nums2, nums1)
+        
+        d = {}
         res = []
-        for a in nums2:
-            if a in dic and dic[a] > 0:
-                res.append(a)
-                dic[a] -= 1
+        for no in nums1:
+          d[no] = d.get(no,0) + 1
+        
+        for no in nums2:
+          if no in d and d[no]:
+            res.append(no)
+            d[no] -= 1
         return res
+
+# Follow Up - 1
+class Solution:
+    # Time Complexity - O(m+n), by assuming arrays are already sorted
+    # Space Complexity - O(1)
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums2) > len(nums1):
+          self.intersect(nums2, nums1)
+        
+        nums1.sort()
+        nums2.sort()
+        p1 = p2 = 0
+        res = []
+        while p1 < len(nums1) and p2 < len(nums2):
+          if nums1[p1] == nums2[p2]:
+            res.append(nums1[p1])
+            p1 += 1
+            p2 += 1
+          elif nums1[p1] < nums2[p2]:
+            p1 += 1
+          else:
+            p2 += 1
+        return res
+
+# Follow Up - 3
+"""
+What if elements of nums2 are stored on disk, and the memory is
+limited such that you cannot load all elements into the memory at
+once?
+
+If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, 
+read chunks of array that fit into the memory, and record the intersections.
+If both nums1 and nums2 are so huge that neither fit into the memory, 
+sort them individually (external sort), then read 2 elements from each 
+array at a time in memory, record intersections.
+"""
