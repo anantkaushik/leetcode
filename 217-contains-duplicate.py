578f7e46cd6c773f49a6fb0ba86feda5044775ef
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-25 19:05:03 +0530

    Contains Duplicate

--- 217-contains-duplicate.py
+++ 217-contains-duplicate.py
@@ -17,7 +17,18 @@ Example 3:
 Input: [1,1,1,3,3,4,3,2,4,2]
 Output: true
 """
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 class Solution(object):
     def containsDuplicate(self, nums):
-        s = set(nums)
-        return len(nums) != len(s)
+        return len(nums) != len(set(nums))
+    
+# Little better solution as if we found duplicate element we break 
+class Solution1:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        temp = set()
+        for i in nums:
+          if i in temp:
+            return True
+          temp.add(i)
+        return False
