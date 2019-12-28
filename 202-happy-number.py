304549563f421669de62114033cab418473a0a9b
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-28 12:19:57 +0530

    Happy Number

--- 202-happy-number.py
+++ 202-happy-number.py
@@ -17,7 +17,24 @@ Explanation:
 62 + 82 = 100
 12 + 02 + 02 = 1
 """
-class Solution(object):
+# Using Floyd Cycle detection algorithm
+class Solution:
+    def isHappy(self, n: int) -> bool:
+      slow = n
+      fast = self.squareSum(self.squareSum(n))
+      while fast != slow:
+        slow = self.squareSum(slow)
+        fast = self.squareSum(self.squareSum(fast))
+      return fast == 1
+        
+    def squareSum(self, n):
+      new_no = 0
+      while n:
+        new_no += (n%10) * (n%10)
+        n //= 10
+      return new_no
+
+class Solution1:
     def isHappy(self, n):
         """
         :type n: int
@@ -30,3 +47,22 @@ class Solution(object):
                 n //= 10
             n = nextN
         return n == 1
+
+class Solution2:
+    def isHappy(self, n: int) -> bool:
+      temp = set()
+      while n:
+        if n == 1:
+          return True
+        temp.add(n)
+        new_no = self.squareSum(n)
+        if new_no in temp:
+          return False
+        n = new_no
+        
+    def squareSum(self, n):
+      new_no = 0
+      while n:
+        new_no += (n%10) * (n%10)
+        n //= 10
+      return new_no
