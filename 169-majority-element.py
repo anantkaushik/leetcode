2051f51f7dc151f212c3830b584fce3b63065a71
Anant Kaushik <anant.kaushik2@gmail.com>
2019-12-29 23:21:48 +0530

    Majority Element

--- 169-majority-element.py
+++ 169-majority-element.py
@@ -1,4 +1,6 @@
 """
+Problem Link: https://leetcode.com/problems/majority-element/
+
 Given an array of size n, find the majority element. 
 The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 You may assume that the array is non-empty and the majority element always exist in the array.
@@ -11,15 +13,11 @@ Example 2:
 Input: [2,2,1,1,1,2,2]
 Output: 2
 """
-class Solution(object):
-    def majorityElement(self, nums):
-        c,ans = 0,0
-        for i in nums:
-            if c == 0:
-                c+=1
-                ans = i
-            elif ans == i:
-                c += 1
-            else:
-                c -= 1
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = ans = 0
+        for n in nums:
+          if count == 0:
+            ans = n
+          count += 1 if ans == n else -1
         return ans
