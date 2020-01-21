2b9b59f239df5a4a5adc7e36f76349e9043dd62f
Anant Kaushik <anant.kaushik2@gmail.com>
2020-01-21 22:44:38 +0530

    Diameter of Binary Tree

--- 543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree.py
@@ -15,23 +15,21 @@ Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
 Note: The length of path between two nodes is represented by the number of edges between them.
 """
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def diameterOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        self.d = 1
-        def depth(node):
-            if not node: return 0
-            l, r = depth(node.left), depth(node.right)
-            self.d = max(self.d, l+r+1)
-            return max(l,r) + 1
-        depth(root)
-        return self.d - 1
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+      self.diameter = 0
+      def depth(root):
+        if not root:
+          return 0
+        l = depth(root.left)
+        r = depth(root.right)
+        self.diameter = max(self.diameter, l+r)
+        return max(l,r)+1
+      depth(root)
+      return self.diameter
