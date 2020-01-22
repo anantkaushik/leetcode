cba16397edae7af1d3cfa03880f21f15856f95af
Anant Kaushik <anant.kaushik2@gmail.com>
2020-01-22 22:18:45 +0530

    To Lower Case

--- 709-to-lowercase.py
+++ 709-to-lowercase.py
@@ -1,7 +1,23 @@
 """
-Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+Problem Link: https://leetcode.com/problems/to-lower-case/
+Implement function ToLowerCase() that has a string parameter str, and returns the same string 
+in lowercase.
+
+Example 1:
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+Input: "here"
+Output: "here"
+
+Example 3:
+Input: "LOVELY"
+Output: "lovely"
 """
 class Solution:
-    def toLowerCase(self, str):
-        return ''.join([chr(ord('a')+ord(s)-ord('A')) if ord(s) <= ord('Z') and ord(s) >= ord('A') else s for s in str])
+    def toLowerCase(self, str: str) -> str:
+        # return ''.join([chr(ord('a')+ord(s)-ord('A')) if ord(s) <= ord('Z') and ord(s) >= ord('A') else s for s in str])
+        return "".join([chr(ord(c)+32) if 65 <= ord(c) <= 90 else c for c in str])
         
+
