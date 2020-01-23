0e9fb5e9906df0c509301e8184ce2bd72a433cc2
Anant Kaushik <anant.kaushik2@gmail.com>
2020-01-23 22:31:13 +0530

    Invert Binary Tree

--- 226-invert-binary-tree.py
+++ 226-invert-binary-tree.py
@@ -21,23 +21,37 @@ Output:
 9   6 3   1
 """
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
+# Time Complexity - O(n), n is the total no of nodes
+# Space Complexity - O(n), Because of recursion, O(h) function calls will be placed on the queue 
+# in the worst case, where h is the height of the tree. Because h in h ∈ O(n), 
+# the space complexity is O(n)
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
         if not root:
-            return None
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+          return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+# Time Complexity - O(n), n is the total no of nodes
+# Space complexity - O(n), since in the worst case, the queue will contain all nodes in one 
+# level of the binary tree. For a full binary tree, the leaf level has n/2 = O(n) leaves.
+class SolutionIterative:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+          return None
+        queue = []
+        queue.append(root)
+        while queue:
+          cur = queue.pop(0)
+          cur.left, cur.right = cur.right, cur.left
+          if cur.left:
+            queue.append(cur.left)
+          if cur.right:
+            queue.append(cur.right)
         return root
