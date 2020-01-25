ffa9bc7d6c04c15ba636a5dd764553ebd30feeaf
Anant Kaushik <anant.kaushik2@gmail.com>
2020-01-25 14:07:35 +0530

    Same Tree

--- 100-same-tree.py
+++ 100-same-tree.py
@@ -29,16 +29,34 @@ Input:     1         1
 Output: false
 """
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def isSameTree(self, p, q):
-        if p == None and q == None:
-            return True
-        if p == None or q == None:
-            return False
-        return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+# Time Complexity: O(n), n -> total no of nodes
+# Space Complexity: O(log(N)) in the best case of completely balanced tree and 
+# O(N) in the worst case of completely unbalanced tree.
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+          return True
+        elif not p or not q:
+          return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def isSameTree_iterative(self, p: TreeNode, q: TreeNode) -> bool:
+      stack = [(p, q)]
+      while stack:
+          node1, node2 = stack.pop()
+          if not node1 and not node2:
+              continue
+          elif not node1 or not node2:
+              return False
+          if node1.val != node2.val:
+              return False
+          stack.append((node1.right, node2.right))
+          stack.append((node1.left, node2.left))
+      return True
