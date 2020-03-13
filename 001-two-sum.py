9d07c686cb221bf67cb893a3fedde8da99247e6e
Anant Kaushik <anant.kaushik2@gmail.com>
2020-03-13 13:03:59 +0530

    Two Sum

--- 001-two-sum.py
+++ 001-two-sum.py
@@ -1,4 +1,5 @@
 """
+Problem Link: https://leetcode.com/problems/two-sum/
 Given an array of integers, 
 return indices of the two numbers such that they add up to a specific target.
 
@@ -10,11 +11,22 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+# Time Complesity: O(N)
+# Sapce Complexoty: O(N)
 class Solution:
-    def twoSum(self, nums, target):
-        n = {}
-        for index,num in enumerate(nums):
-            temp = target - num
-            if temp in n:
-                return [n[temp],index]
-            n[num] = index
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      d = {}
+      for i in range(len(nums)):
+        difference = target - nums[i]
+        if difference in d:
+          return [d[difference], i]
+        d[nums[i]] = i
+
+# Time Complesity: O(N*N)
+# Sapce Complexoty: O(1)
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+          if nums[i] + nums[j] == target:
+            return [i, j]
