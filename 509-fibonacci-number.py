1bc04131180fb7e7831fa5aabf22ab77efb1d8da
Anant Kaushik <anant.kaushik2@gmail.com>
2020-03-13 13:20:02 +0530

    Fibonacci Number

--- 509-fibonacci-number.py
+++ 509-fibonacci-number.py
@@ -35,3 +35,28 @@ class Solution:
         for _ in range(2, N+1):
           a, b = b, a + b
         return b
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+class Solution2:
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        return self.memoize(N)
+
+    def memoize(self, N: int) -> {}:
+        cache = {0: 0, 1: 1}
+
+        # Since range is exclusive and we want to include N, we need to put N+1.
+        for i in range(2, N+1):
+            cache[i] = cache[i-1] + cache[i-2]
+
+        return cache[N]
+       
+# Time complexity: O(2^N)
+# Space complexity: O(N)
+class Solution1:
+    def fib(self, N: int) -> int:
+        if N <= 1:
+            return N
+        return self.fib(N-1) + self.fib(N-2)
