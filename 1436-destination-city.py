42cf51f0ab876263ab13602c04808f4286f06616
Anant Kaushik <anant.kaushik2@gmail.com>
2020-05-05 21:49:50 +0530

    Destination City

---
+++ 1436-destination-city.py
@@ -0,0 +1,49 @@
+"""
+Problem Link: https://leetcode.com/problems/destination-city/
+
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct 
+path going from cityAi to cityBi. Return the destination city, that is, the city without any 
+path outgoing to another city.
+It is guaranteed that the graph of paths forms a line without any loop, therefore, 
+there will be exactly one destination city.
+
+Example 1:
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo" 
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. 
+Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+
+Example 2:
+Input: paths = [["B","C"],["D","B"],["C","A"]]
+Output: "A"
+Explanation: All possible trips are: 
+"D" -> "B" -> "C" -> "A". 
+"B" -> "C" -> "A". 
+"C" -> "A". 
+"A". 
+Clearly the destination city is "A".
+
+Example 3:
+Input: paths = [["A","Z"]]
+Output: "Z"
+ 
+Constraints:
+1 <= paths.length <= 100
+paths[i].length == 2
+1 <= cityAi.length, cityBi.length <= 10
+cityAi != cityBi
+All strings consist of lowercase and uppercase English letters and the space character.
+"""
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        temp = set()
+        res = set()
+        
+        for path in paths:
+          temp.add(path[0])
+          if path[0] in res:
+            res.remove(path[0])
+            
+          if path[1] not in temp:
+            res.add(path[1])
+        return res.pop()
