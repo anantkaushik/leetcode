0fc2c65ba34f60f9b5b8f68cefe9240af1db3466
Anant Kaushik <anant.kaushik2@gmail.com>
2020-05-12 22:25:07 +0530

    Reverse String

--- 344-reverse-string.py
+++ 344-reverse-string.py
@@ -1,11 +1,26 @@
 """
-Write a function that takes a string as input and returns the string reversed.
+Problem Link: https://leetcode.com/problems/reverse-string/
 
-Example:
-Given s = "hello", return "olleh".
+Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array 
+in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters.
 
-Run Time: 48ms
+Example 1:
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
 """
 class Solution:
-    def reverseString(self, s):
-        return s[::-1]
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start, end = 0, len(s) - 1
+        while start < end:
+          s[start], s[end] = s[end], s[start]
+          start += 1
+          end -= 1
