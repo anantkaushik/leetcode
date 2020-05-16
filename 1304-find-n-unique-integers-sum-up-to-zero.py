3083f32b6e576246e911adbac23a3a8b4af43e79
Anant Kaushik <anant.kaushik2@gmail.com>
2020-05-16 23:55:21 +0530

    Find N Unique Integers Sum up to Zero

--- 1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero.py
@@ -30,3 +30,18 @@ class Solution:
           res.extend([-i, i])
         
         return res
+
+"""
+n = 1, [0]
+n = 2, [-1, 1]
+n = 3, [-2, 0, 2]
+n = 4, [-3, -1, 1, 3]
+n = 5, [-4, -2, 0, 2, 4]
+
+Explanation
+Find the rule
+A[i] = i * 2 - n + 1
+"""
+class Solution1:
+    def sumZero(self, n: int) -> List[int]:
+        return [i for i in range(1 - n, n, 2)]
