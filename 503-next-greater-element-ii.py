498cc9495ed0669b9f45404627e1437fbccd381e
Anant Kaushik <anant.kaushik2@gmail.com>
2020-05-29 09:06:23 +0530

    Next Greater Element II

---
+++ 503-next-greater-element-ii.py
@@ -0,0 +1,31 @@
+"""
+Problem Link: https://leetcode.com/problems/next-greater-element-ii/
+
+Given a circular array (the next element of the last element is the first element of the array), 
+print the Next Greater Number for every element. The Next Greater Number of a number x is 
+the first greater number to its traversing-order next in the array, which means you could 
+search circularly to find its next greater number. If it doesn't exist, output -1 for 
+this number.
+
+Example 1:
+Input: [1,2,1]
+Output: [2,-1,2]
+Explanation: The first 1's next greater number is 2; 
+The number 2 can't find next greater number; 
+The second 1's next greater number needs to search circularly, which is also 2.
+
+Note: The length of given array won't exceed 10000.
+"""
+class Solution:
+    # Space Complexity: O(N)
+    # Time Complexity: O(N)
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+      stack, res = [], [-1] * len(nums)
+      for i in range(2*len(nums)-1, -1, -1):
+        index = i % len(nums)
+        while stack and nums[stack[-1]] <= nums[index]:
+          stack.pop()
+          
+        res[index] = nums[stack[-1]] if stack else -1
+        stack.append(index)
+      return res
