d2924f0fb5106e0e5be740e37be4364b8ef3db30
Anant Kaushik <anant.kaushik2@gmail.com>
2020-08-10 09:57:21 +0530

    Pascals Triangle II

--- 119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii.py
@@ -16,8 +16,8 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         rowIndex += 1
         res = [0] * rowIndex
-        res[0] = 1;
+        res[0] = 1
         for i in range(1, rowIndex):
             for j in range(i, 0, -1):
-                res[j] += res[j-1];
+                res[j] += res[j-1]
         return res
