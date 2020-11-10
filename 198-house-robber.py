aea48da052c612ae84c32e2e9a7e22c2ce10a401
Anant Kaushik <anant.kaushik2@gmail.com>
2020-11-10 16:05:09 +0530

    House Robber

--- 198-house-robber.py
+++ 198-house-robber.py
@@ -20,15 +20,62 @@ Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
              Total amount you can rob = 2 + 9 + 1 = 12.
 """
-class Solution(object):
-    def rob(self, nums):
-        l = len(nums)
-        if l == 0: return 0
-        elif l == 1: return nums[0]
-        prev2, prev1 = nums[0], max(nums[0], nums[1])
+# BOTTOM UP APPROACH + 2 Variables
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        prev1 = prev = 0
+        
+        for num in nums:
+            prev1, prev = prev, max(prev1 + num, prev)
+        
+        return prev
 
-        for i in range(2, l):
-            curr = max(prev1, prev2 + nums[i])
-            prev2 = prev1
-            prev1 = curr
-        return prev1
+
+# BOTTOM UP APPROACH + MEMO
+class Solution3:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        
+        memo = [-1] * len(nums)
+        memo[0] = nums[0]
+        memo[1] = max(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            memo[i] = max(memo[i-2] + nums[i], memo[i-1])
+        
+        return memo[-1]
+        
+
+# TOP DOWN APPROACH + MEMO
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        self.dp = [-1] * len(nums)
+        return self.helper(nums, len(nums)-1)
+        
+    def helper(self, nums, index):
+        if index < 0:
+            return 0
+        
+        if self.dp[index] != -1:
+            return self.dp[index]
+        
+        self.dp[index] = max(self.helper(nums, index-2) + nums[index], self.helper(nums, index-1))
+        return self.dp[index]
+
+
+# TOP DOWN APPROACH
+# TLE
+class Solution1:
+    def rob(self, nums: List[int]) -> int:
+        return self.helper(nums, len(nums)-1)
+        
+    def helper(self, nums, index):
+        if index < 0:
+            return 0
+        return max(self.helper(nums, index-2) + nums[index], self.helper(nums, index-1))
