bc097f13b23e9e8cc99d7daad0828c365d86a7af
anant <anant.kaushik2@gmail.com>
2021-11-25 18:07:52 +0530

    N-th Tribonacci Number

--- 1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number.py
@@ -23,13 +23,33 @@ The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1
 """
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0 
-        elif n == 1 or n == 2:
+        if n <= 1:
+            return n
+        elif n == 2:
             return 1
-        a = 0
-        b = c = 1
-        for i in range(2,n):
-            ans = a+b+c
-            a,b,c = b,c,ans
-        return ans
+        
+        first, second, third = 0, 1, 1
+        for _ in range(3, n+1):
+            first, second, third = second, third, first + second + third
+        
+        return third
+
+
+class Solution1:
+    def tribonacci(self, n: int) -> int:
+        return self.helper(n, {0:0, 1:1, 2:1})
+    
+    def helper(self, n, memo):
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo) + self.helper(n-3, memo)
+        return memo[n]
+
+
+# TLE
+class Solution2:
+    def tribonacci(self, n: int) -> int:
+        if n <= 1:
+            return n
+        elif n == 2:
+            return 1
+        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
