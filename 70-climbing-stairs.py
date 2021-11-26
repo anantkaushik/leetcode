43b8ac1846bd95c5c9e4d149d00877d4f62caa4c
anant <anant.kaushik2@gmail.com>
2021-11-26 21:47:26 +0530

    Climbing Stairs

--- 70-climbing-stairs.py
+++ 70-climbing-stairs.py
@@ -22,11 +22,36 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 """
-class Solution(object):
-    def climbStairs(self, n):
-        if n  == 1:
-            return 1
-        a,b = 1,2
-        for _ in range(2,n):
-            a,b = b, a+b
-        return b
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        second_last_stair, last_stair = 1, 2 
+        
+        for _ in range(3, n+1):
+            second_last_stair, last_stair = last_stair, last_stair + second_last_stair
+        
+        return last_stair
+
+
+class Solution1:
+    def climbStairs(self, n: int) -> int:
+        return self.helper(n, {})
+        
+    def helper(self, n, memo):
+        if n <= 2:
+            return n
+        
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        
+        return memo[n]
+
+
+# TLE
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
